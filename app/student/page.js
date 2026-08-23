8e13fa24157e79c6125a6e23e96a653003eec488
DIVINE039 <adityakverma945085@gmail.com>
2026-08-23 13:11:15 +0530

    Update gh-pages with Sem 5 & 6 combined leaderboard

--- app/student/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { STUDENTS } from '@/lib/data';
-import StudentDashboard from '@/components/StudentDashboard';
-
-export default function StudentIndexPage() {
-  const defaultCse = STUDENTS.find(s => s.branch === 'CSE') || STUDENTS[0];
-  return <StudentDashboard rollNo={defaultCse.rollNo} />;
-}
--- scraper/inspect_rohit_sem4.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open(r"E:\result of All eie\EIE_sem_IV\231351139011_EIE_sem_IV.html", 'r', encoding='utf-8') as f:
-    soup = BeautifulSoup(f.read(), 'html.parser')
-
-print("Candidate Name:", soup.find('span', id='lblCandidateName'))
-print("Roll:", soup.find('span', id='lblRollNo'))
-print("Course:", soup.find('span', id='lblCourseName'))
-print("Error msg:", soup.find('span', id=lambda x: x and 'msg' in x.lower()))
