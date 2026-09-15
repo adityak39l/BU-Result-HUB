a18a7d3c0a1b039cb0c5dab5d6a9fb38f7973b09
DIVINE039 <adityakverma945085@gmail.com>
2026-09-15 10:00:05 +0530

    Deploy SEO-optimized build to gh-pages

---
+++ _next/static/KoZtZIeznaPlujTTj_JdI/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fstudent\u002F[rollNo]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/student/[rollNo]/page-bb7b8828799cfc11.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[333],{3622:function(n,e,u){Promise.resolve().then(u.bind(u,7238))}},function(n){n.O(0,[250,569,238,971,69,744],function(){return n(n.s=3622)}),_N_E=n.O()}]);
